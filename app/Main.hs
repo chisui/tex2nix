7ef13516a17f89c91ea67f59f2a80d531fe49704
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-10-02 23:52:18 +0200

    temporarily split index and dump

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
-import "base" Control.Monad ((<=<))
-import "base" Data.Maybe (listToMaybe)
+import "base" Control.Monad ((<=<), (>=>))
+
 import "containers" Data.Set (Set)
 import qualified "containers" Data.Set as Set
 
-import "optparse-applicative" Options.Applicative (Parser, (<**>), info, helper, fullDesc, argument, str, metavar, execParser) 
+import "optparse-applicative" Options.Applicative (ParserInfo, (<**>), fullDesc, helper, subparser, command, info, progDesc, argument, str, metavar, execParser) 
 
 import "HaTeX" Text.LaTeX.Base.Parser (parseLaTeXFile)
 import "HaTeX" Text.LaTeX.Base.Syntax (LaTeX(..), TeXArg(..), lookForCommand)
@@ -15,24 +15,35 @@ import "HaTeX" Text.LaTeX.Base.Render (render)
 import "text" Data.Text (Text)
 import qualified "text" Data.Text as Text
 
-data Args = Args
-    { file :: FilePath
-    }
+data Args 
+    = DumpPackagesArgs
+        { file :: FilePath
+        }
+    | IndexDbArgs
+        { file :: FilePath
+        }
   deriving (Eq, Ord, Show)
 
-argsParser :: Parser Args
-argsParser = Args
-    <$> (argument str (metavar "FILE"))
-
-opts = info (argsParser <**> helper) fullDesc
+argsParser :: ParserInfo Args
+argsParser = info (cmds <**> helper) fullDesc
+  where
+    cmds = subparser $ mconcat
+        [ command "dump"  (info dump  $ progDesc "dump package names of tex file")
+        , command "index" (info index $ progDesc "index texlive package database")
+        ]
+    dump  = DumpPackagesArgs <$> argument str (metavar "FILE")
+    index = IndexDbArgs      <$> argument str (metavar "FILE")
 
 main :: IO ()
-main = do
-    Args{ file } <- execParser opts
-    res <- parseLaTeXFile file
-    case res of
+main = execParser argsParser >>= \case
+    DumpPackagesArgs{ file } -> dumpPkgs file
+    IndexDbArgs{ file } -> indexDb file
+  where
+    dumpPkgs = parseLaTeXFile >=> \case
       Left err -> error $ show err
       Right doc -> mapM_ print . packageNames $ doc
+    indexDb file = do
+      putStrLn file
 
 packageNames :: LaTeX -> Set Text
 packageNames = Set.fromList . (Text.splitOn "," <=< (pkgName =<<) <=< lookForCommand "usepackage")
